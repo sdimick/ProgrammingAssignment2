3215ab36e08007f5d79c71e1dc7537a5f1965d25
Seth Dimick <seth.dimick@nordstrom.com>
2017-10-11 21:14:10 -0700

    change comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,11 @@
-## The two folowing functions are used together to skip the process
-## of inversing a matrix if you have already solved the matrix.
+# The two folowing functions are used together to skip the process
+# of inversing a matrix if you have already solved the matrix.
 
-## ---------------------------------------------------------------
+# 'makeCacheMatrix' transforms a matrix into 'special matrix', which
+# is really a list, that will stores function and objects that will be
+# used by the 'cacheSolve' function
 
-## 'makeCacheMatrix' transforms a matrix into 'special matrix', which
-## is really a list, that will stores function and objects that will be
-## used by the 'cacheSolve' function
-
-## takes one argument, x, which is a matrix
+# takes one argument, x, which is a matrix
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -22,15 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-## ---------------------------------------------------------------
+# 'casheSolve' returnes the inverse of the 'specail matrix' created
+# by 'makeCacheMatrix' by either a) printing the inverse from memory 
+# if it has previously been solved, or b) solving the matrix and then
+# returning it.
 
-## casheSolve returnes the inverse of the 'specail matrix' created
-## by 'makeCacheMatrix' by either a) printing the inverse from memory 
-## if it has previously been solved, or b) solving the matrix and then
-## returning it.
-
-## 'cacheSolve' takes one argument, x, which is the 'specail matrix'
-## created by 'makeCacheMatrix'
+# 'cacheSolve' takes one argument, x, a 'specail matrix' created by 
+# 'makeCacheMatrix'
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
